e6fe5f29e3cb5040d21ce6bd2af8f3ab51aa2a53
Andy <andranadu@gmail.com>
2018-11-25 11:47:28 +0000

    Weighted sort on keyword results

--- src/modules/keyword.js
+++ src/modules/keyword.js
@@ -17,20 +17,18 @@ class KeywordBoi {
 
     static getIncidencesInStr (str) {
         let words = str.toLowerCase().match(/\S+/g).sort();
-        console.log(words)
         let wordNo = words.length; 
-        let counts = {};
+        let counts = [];
         let currWord = '';
         let currCount = 0;
         for (let i of words) {
-            console.log(i)
             if (i === currWord) currCount++;
             else {
-                if (currCount !== 0) counts[currWord] = currCount;
+                if (currCount !== 0) counts.push([currWord,currCount]);
                 currWord = i; currCount = 1;
             }
         }
-        if (currCount !== 0) counts[currWord] = currCount;
+        if (currCount !== 0) counts.push([currWord,currCount]);
         return counts;
     }
 
@@ -41,12 +39,17 @@ class KeywordBoi {
     static getIncidencesWeighted (str) {
         let incidences = this.getIncidencesInStr(str);
         for (let w of incidences) {
-            incidences[w] /= this.getIncidenceByWord(w);
+            let weight = this.getIncidenceByWord(w[0]);
+            if (weight === 0) w[1] = 0;
+            else w[1] /= weight;
         }
         return incidences;
     }
 
+    static getKeywordsFromStr(str) {
+        return this.getIncidencesWeighted(str).sort((a,b) => b[1] - a[1]).splice(0,3).map(x => x[0]);
+    }
+
 }
 
-//
-console.log(KeywordBoi.getIncidenceByWord("hello"));
+//console.log(KeywordBoi.getKeywordsFromStr("hello hi hello hey hello hello hi when i go to the moon i go to the sky sky sky sky sky sky"));
