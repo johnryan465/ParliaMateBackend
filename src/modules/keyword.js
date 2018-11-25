6258a7231369abf429cacc444c7eabf0a7550a88
Andy <andranadu@gmail.com>
2018-11-25 00:36:54 +0000

    Keywords n stuff
    KeywordBoi takes incidences of words in a given string and compares their relative frequency to that of a wikipedia generated table of word frequencies

---
+++ src/modules/keyword.js
@@ -0,0 +1,51 @@
+
+const fs = require('fs');
+
+const WordCache = (() => {
+    let r = {}
+    fs.readFileSync('./kw_incidences.txt')  
+      .toString()
+      .match(/[^\n]+/g)
+      .map(x => x.split(/\s/))
+      .forEach(x => {
+          r[x[0]] = parseInt(x[1])
+      });
+    return r
+})();
+
+class KeywordBoi {
+
+    static getIncidencesInStr (str) {
+        let words = str.toLowerCase().match(/\S+/g).sort();
+        console.log(words)
+        let wordNo = words.length; 
+        let counts = {};
+        let currWord = '';
+        let currCount = 0;
+        for (let i of words) {
+            console.log(i)
+            if (i === currWord) currCount++;
+            else {
+                if (currCount !== 0) counts[currWord] = currCount;
+                currWord = i; currCount = 1;
+            }
+        }
+        if (currCount !== 0) counts[currWord] = currCount;
+        return counts;
+    }
+
+    static getIncidenceByWord (word) { 
+        return WordCache[word] || 0; 
+    }
+
+    static getIncidencesWeighted (str) {
+        let incidences = this.getIncidencesInStr(str);
+        for (let w of incidences) {
+            incidences[w] /= this.getIncidenceByWord(w);
+        }
+        return incidences;
+    }
+
+}
+
+//console.log(KeywordBoi.getIncidenceByWord("hello"));
